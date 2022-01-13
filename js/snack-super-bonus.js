63d562f8419ddb44298800944d0295bea2f2a5b9
vrossi96 <vrossi396@yahoo.it>
2022-01-13 18:28:55 +0100

    snack 2 super bonus done

--- js/snack-super-bonus.js
+++ js/snack-super-bonus.js
@@ -5,6 +5,9 @@ let i = 0;
 const s1Input = document.getElementById('s1-input');
 const s1Calculate = document.getElementById('s1-calculate');
 const s1Result = document.getElementById('sbonus-s1-result');
+const s2Input = document.getElementById('s2-input');
+const s2Calculate = document.getElementById('s2-calculate');
+const s2Result = document.getElementById('sbonus-s2-result');
 
 /*
 Il software deve chiedere per 5 volte all’utente di inserire un numero.
@@ -35,3 +38,33 @@ s1Calculate.addEventListener('click', function() {
 })
 
 //! Snack 2
+/*
+Inserisci un numero, se è pari stampa il numero,
+se è dispari stampa il numero successivo
+*/
+
+s2Calculate.addEventListener('click', function() {
+  const number = parseInt(s2Input.value);
+  if (number % 2 == 0) {
+    s2Result.innerText = number;
+  } else {      // Se no stampo il successivo
+    s2Result.innerText = number + 1;
+  }
+})
+
+
+/*
+let check = false;
+
+while (check == false) {
+  // Chiedo all'utente un numero
+  const number = parseInt(prompt('Inserisci un numero', 3));
+  // Se è pari lo stampo
+  if (number % 2 == 0) {
+    bonusS2Result.innerText = number;
+  } else {      // Se no stampo il successivo
+    bonusS2Result.innerText = number + 1;
+  }
+  check = true;
+}
+*/
