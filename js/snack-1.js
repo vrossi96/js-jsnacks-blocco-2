8c1737570afe5b510a15e2ce7a9ebced901346fd
vrossi96 <vrossi396@yahoo.it>
2022-01-13 16:28:43 +0100

    add in page snack 2

--- js/snack-1.js
+++ js/snack-1.js
@@ -18,3 +18,4 @@ while (i <= 5) {
 }
 
 console.log(sum);
+
--- js/snack-bonus.js
+++ js/snack-bonus.js
@@ -6,10 +6,12 @@ Esegui questo programma in due versioni, con il for e con il while.
 
 // Bonus show
 const bonusS1Result = document.getElementById('bonus-s1-result');
+const bonusS2Result = document.getElementById('bonus-s2-result');
 
 let i = 1;
 let bonusS1Sum = 0;
 
+//! Snack 1
 while (i <= 5) {
   // Chiedo all'utente i numeri per 5 volte
   const number = parseInt(prompt('Inserisci un numero'));
@@ -20,3 +22,18 @@ while (i <= 5) {
 }
 
 bonusS1Result.innerText = bonusS1Sum;
+
+//! Snack 2
+let check = false;
+
+while (check == false) {
+  // Chiedo all'utente un numero
+  const number = parseInt(prompt('Inserisci un numero'));
+  // Se è pari lo stampo
+  if (number % 2 == 0) {
+    bonusS2Result.innerText = number;
+  } else {      // Se no stampo il successivo
+    bonusS2Result.innerText = number + 1;
+  }
+  check = true;
+}
