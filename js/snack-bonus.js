5e77c4d180488966b199c1f60760a59d64fbeb94
vrossi96 <vrossi396@yahoo.it>
2022-01-14 09:51:49 +0100

    changed variable type in some arrays

--- js/snack-bonus.js
+++ js/snack-bonus.js
@@ -60,7 +60,7 @@ lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
 
 const arrayName = ['Luigi', 'Mario', 'Wario', 'Waluigi', 'Gigi', 'Bastone', 'Gastone', 'Paolino'];
 const arraySurname = ['Pigna', 'Duro', 'Bello', 'Brutto', 'Bestia', 'Rovina', 'Ruperta', 'Blob'];
-let namesMerged = [];
+const namesMerged = [];
 
 
 while (namesMerged.length < 3) {
@@ -103,8 +103,8 @@ Aggiungi elementi casuali all’array che ha meno elementi,
 fino a quando ne avrà tanti quanti l’altro.
 */
 
-let array1 = [15, 25, 75, 28, 33, 58];
-let array2 = [15, 25, 75, 28, 33, 58, 85, 99, 69];
+const array1 = [15, 25, 75, 28, 33, 58];
+const array2 = [15, 25, 75, 28, 33, 58, 85, 99, 69];
 
 // Fin che sono di diversa lunghezza allora eseguo i comandi all'interno
 while (array1.length !== array2.length) {
